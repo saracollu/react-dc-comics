55b124d8ffc85acb2e5e2e644caaf6d7e5837350
saracollu <saracollu.ws@gmail.com>
2026-04-26 21:34:20 +0200

    started css form header

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'*/
 
+
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
